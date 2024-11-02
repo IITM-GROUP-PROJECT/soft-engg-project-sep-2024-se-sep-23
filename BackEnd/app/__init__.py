3fd483873578b2ec566ad0dc9e49140efb83f3fb
kannan-14926 <kannan.saravanan@zohocorp.com>
2024-11-02 14:14:47 +0530

    Complete Project Struture setting up logging

--- BackEnd/app/__init__.py
+++ BackEnd/app/__init__.py
@@ -4,14 +4,15 @@ from flask import Flask
 from .extensions import db, migrate
 from config import Config
 from .routes.api_routes import api_routes
+from config import setup_logging
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
 
+    setup_logging(app)
+    app.config.from_object(Config)
     db.init_app(app)
     migrate.init_app(app, db)
-
 
     app.register_blueprint(api_routes)
 
--- BackEnd/config.py
+++ BackEnd/config.py
@@ -1,7 +1,45 @@
 import os
+import logging
 from dotenv import load_dotenv
+from logging.handlers import TimedRotatingFileHandler
+from datetime import datetime
+
+
 
 load_dotenv()
+
+LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG").upper()
+LOG_DIRECTORY = "logs"
+
+
+if not os.path.exists(LOG_DIRECTORY):
+    os.makedirs(LOG_DIRECTORY)
+
+class CustomTimedRotatingFileHandler(TimedRotatingFileHandler):
+    def doRollover(self):
+        super().doRollover()
+        new_file_name = "app_"+datetime.now().strftime("%Y-%m-%d") + ".log"
+        old_file_path = self.baseFilename
+        new_file_path = os.path.join(LOG_DIRECTORY, new_file_name)
+        os.rename(old_file_path, new_file_path)
+
+
+handler = CustomTimedRotatingFileHandler(
+    filename=os.path.join(LOG_DIRECTORY, "app.log"),
+    when="midnight",
+    interval=1,
+    backupCount=5,
+)
+handler.setLevel(LOG_LEVEL)
+handler.setFormatter(logging.Formatter(
+    "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+))
+
+
+def setup_logging(app):
+    app.logger.setLevel(LOG_LEVEL)
+    app.logger.addHandler(handler)
+
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
