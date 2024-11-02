76bbda70f53ded0cd4357e26b6853ea47f2979ba
kannan-14926 <kannan.saravanan@zohocorp.com>
2024-11-02 17:31:41 +0530

    gemini integration methods

--- BackEnd/app/services/logic.py
+++ BackEnd/app/services/logic.py
@@ -1,1 +1,27 @@
 # here we will create all business logic like how we process all app data
+from app.utils import helpers
+import json
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+def process_project_report(filepath):
+    report = helpers.pdf_to_text(filepath)
+    guidelines = load_static_data().report_eval_prompt
+    data = {"report":report,"guidelines": guidelines}
+    response = helpers.ai_eval(data)
+
+    if not response.status:
+        return "Error while processing report with Gemini, Contact Support "
+    else:
+        return  response.text
+
+
+
+def load_static_data():
+    with open(os.getenv("APP_DATA_JSON_PATH", "app/resources/application_data.json")) as file:
+        data = json.load(file)
+    return data
--- BackEnd/app/utils/helpers.py
+++ BackEnd/app/utils/helpers.py
@@ -1,1 +1,34 @@
 # you can use this method to create common methods like encoder/decoder
+import fitz
+import os
+import google.generativeai as genai
+
+
+def ai_eval(data):
+    try:
+
+        api_key = os.getenv('GEMINI_API_KEY')
+        if not api_key:
+            raise ValueError("GEMINI_API_KEY not found in environment variables")
+        genai.configure(api_key=api_key)
+        model = genai.GenerativeModel("gemini-1.5-flash")
+
+        report = data.get('report')
+        guidelines = data.get('guidelines')
+
+        prompt = f"Evaluate the following Data based on the Guidelines. Guidelines: {guidelines}. Data: {report}"
+        response = model.generate_content(prompt)
+        response.status= True
+        return response
+    except Exception as e:
+        response = {'error':str(e),'status':False}
+        return response
+
+
+def pdf_to_text(filepath):
+    text = ""
+    with fitz.open(filepath) as pdf_document:
+        for page_num in range(pdf_document.page_count):
+            page = pdf_document[page_num]
+            text += page.get_text("text")
+    return text
